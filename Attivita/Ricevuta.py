6ec612c82b79077b12694df95699f025270e034f
Alessandro Renzi <alessandrorenzi00@gmail.com>
2022-06-21 10:06:12 +0200

    Creazione dell'init

---
+++ Attivita/Ricevuta.py
@@ -0,0 +1,13 @@
+import datetime
+import os
+import pickle
+
+class Ricevuta:
+
+    def __init__(self):
+        self.id = 0
+        self.importo = 0.0
+        self.data = datetime.date(1970, 1, 1)
+        self.ora = datetime.time(0, 0)
+
+    def
