02bd0ceadb45fbdb522ea0e0d01b459e0fbe2d57
Alessandro Renzi <alessandrorenzi00@gmail.com>
2022-06-21 16:05:31 +0200

    Creazione della vista per la home del paziente

---
+++ Viste/VistaHomePaziente.py
@@ -0,0 +1,23 @@
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
+
+class VistaHomePaziente(QWidget):
+
+    def __init__(self, parent = None):
+        super(VistaHomePaziente, self).__init__(parent)
+        grid_layout = QGridLayout()
+        grid_layout.addWidget(self.get_generic_button("Vedi Prenotazioni",self.go), 0, 0)
+        grid_layout.addWidget(self.get_generic_button("Modifica Informazioni", self.go), 0, 0)
+        grid_layout.addWidget(self.get_generic_button("Logout", self.go), 0, 0)
+        self.setLayout(grid_layout)
+        self.resize(400, 300)
+        self.setWindowTitle("Paziente")
+
+        def get_generic_button(self, titolo, on_click):
+            button = QPushButton(titolo)
+            button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            button.clicked.connect(on_click)
+            return button
+
+
+
+
