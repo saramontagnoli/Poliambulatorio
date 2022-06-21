a00b24516747bc0c0ad5b27055ec4056a6cf4a22
saramontagnoli <s1098041@studenti.univpm.it>
2022-06-21 11:10:03 +0200

    Creazione classe per Paziente, sviluppo iniziale costruttore e setInfoPaziente

--- Attivita/Paziente.py
+++ Attivita/Paziente.py
@@ -1,1 +1,24 @@
-#prova
+import os.path
+import pickle
+
+from Attivita.Utilizzatore import Utilizzatore
+
+class Paziente(Utilizzatore):
+
+    def __init__(self):
+        super().__init__()
+        self.prenotazioni = []
+        self.allergia = False
+        self.malattia_pregressa = False
+
+    def setInfoPaziente(self, nome, cognome, password, data_nascita, CF, telefono, genere, mail, indirizzo, nota, allergia, malattia_pregressa):
+        self.setInfoUtilizzatore(nome, cognome, password, data_nascita, CF, telefono, genere, mail, indirizzo, nota)
+        self.allergia = allergia
+        self.malattia_pregressa = malattia_pregressa
+        pazienti = {}
+        if os.path.isfile('File/Pazienti.pickle'):
+            with open('File/Pazienti.pickle', 'rb') as f:
+                pazienti = pickle.load(f)
+        pazienti[id] = self
+        with open('File/Pazienti.pickle', 'wb') as f:
+            pickle.dump(pazienti, f, pickle.HIGHEST_PROTOCOL)
