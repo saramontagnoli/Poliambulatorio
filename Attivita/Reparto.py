c6a5e90607a8dd1beecc9274191249fb3b21493d
Alessandro Renzi <alessandrorenzi00@gmail.com>
2022-06-21 10:42:04 +0200

    Definizione metodi get e set degli attributi

--- Attivita/Reparto.py
+++ Attivita/Reparto.py
@@ -4,4 +4,15 @@ import pickle
 
 class Reparto:
 
+    #metodi set e get dei vari attributi
+    def getId(self):
+        return self.id
 
+    def getNome(self):
+        return self.nome
+
+    def setNota(self, nota):
+        self.nota = nota
+
+    def getNota(self):
+        return self.nota
