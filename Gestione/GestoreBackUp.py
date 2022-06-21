e3369ae6f202d41125af40b8eaa9c2e7ef950825
PCristian00 <cristianp9300@gmail.com>
2022-06-21 15:17:13 +0200

    bozza

---
+++ Gestione/GestoreBackUp.py
@@ -0,0 +1,29 @@
+class GestoreBackUp:
+    def copiaDatiMora(self):
+        #boolean
+        return
+
+    def copiaDatiPrenotazione(self):
+        # boolean
+        return
+
+    def copiaDatiReferto(self):
+        # boolean
+        return
+
+    def copiaDatiRicevuta(self):
+        # boolean
+        return
+
+    def copiaDatiUtilizzatore(self):
+        # boolean
+        return
+
+    def effettuaBackup(self):
+        #void
+        return
+
+    # frequenza intesa come intervallo orario (ogni n ore)
+    def modificaBackUp(self,date,frequenza):
+        # boolean
+        return
