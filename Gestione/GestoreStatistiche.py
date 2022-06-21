e265e3dd0ecb2852aea49ba8f8ac6e82925b47e2
saramontagnoli <s1098041@studenti.univpm.it>
2022-06-21 15:09:52 +0200

    Creazione classe e base dei metodi per la richiesta delle statistiche

---
+++ Gestione/GestoreStatistiche.py
@@ -0,0 +1,18 @@
+import datetime
+import os
+import pickle
+
+class GestoreStatistica:
+
+    def richiediStatisticheMora(self):
+        return
+
+    def richiediStatisticheRicevuta(self):
+        return
+
+    def elaboraDatiMora(self):
+        return
+
+    def elaboraDatiRicevuta(self):
+        return
+
