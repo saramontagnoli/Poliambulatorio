e9a4e7556529a62278466cbb1a6a4dffb0f1321c
saramontagnoli <s1098041@studenti.univpm.it>
2022-06-21 16:01:30 +0200

    Creazione della vista home dell'amministratore e base della struttura della classe

---
+++ Viste/VistaHomeAmm.py
@@ -0,0 +1,43 @@
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
+
+#from Viste.VistaGestisciPazienti import VistaGestisciPazienti
+
+
+class VistaHomeAmm(QWidget):
+
+    #Bottoni con nome e relativo ONLICK, visualizzazione della finestra home del paziente
+    def __init__(self, parent=None):
+        super(VistaHomeAmm, self).__init__(parent)
+        grid_layout = QGridLayout()
+        grid_layout.addWidget(self.get_generic_button("Gestisci Prenotazioni", self.go_prenotazioni), 0, 0)
+        grid_layout.addWidget(self.get_generic_button("Gestisci Pazienti", self.go_pazienti), 0, 1)
+        grid_layout.addWidget(self.get_generic_button("Gestisci Medici", self.go_medici), 1, 0)
+        grid_layout.addWidget(self.get_generic_button("Gestisci Statistiche", self.go_statistiche), 1, 1)
+        grid_layout.addWidget(self.get_generic_button("Gestisci BackUp", self.go_backup), 2, 0, 1, 2)
+        self.setLayout(grid_layout)
+        self.resize(400, 300)
+        self.setWindowTitle("ADMIN")
+
+    #Funzionalità del bottone
+    def get_generic_button(self, titolo, on_click):
+        button = QPushButton(titolo)
+        button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        button.clicked.connect(on_click)
+        return button
+
+    #Metodi onclick
+    def go_prenotazioni(self):
+        pass
+
+    def go_pazienti(self):
+        self.vista_gestisci_pazienti = VistaGestisciPazienti()
+        self.vista_gestisci_pazienti.show()
+
+    def go_medici(self):
+        pass
+
+    def go_statistiche(self):
+        pass
+
+    def go_backup(self):
+        pass
