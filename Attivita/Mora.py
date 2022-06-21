29c724908567d70a6cf37b235755b5513e300045
Alessandro Renzi <alessandrorenzi00@gmail.com>
2022-06-21 11:05:49 +0200

    Creazione della classe Mora e definizione dei metodi get e set per i vari attributi

---
+++ Attivita/Mora.py
@@ -0,0 +1,36 @@
+import datetime
+import os
+import pickle
+
+class Mora:
+
+    def __init__(self):
+        self.id = 0
+        self.importo = 0.0
+        self.nota = ''
+        self.data_emissione = datetime(1970, 0, 0, 0)
+
+    #metodi get e set dei vari attributi
+    def getId(self):
+        return self.id
+
+    def setImporto(self, importo):
+        self.importo = importo
+
+    def getImporto(self):
+        return self.importo
+
+    def setNota(self, nota):
+        self.nota = nota
+
+    def getNota(self):
+        return self.nota
+
+    def setData_emissione(self, data_emissione):
+        self.data_emissione = data_emissione
+
+    def getData_emissione(self):
+        return self.data_emissione
+
+    def stampa(self):
+        return
