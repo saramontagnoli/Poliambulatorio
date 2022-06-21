0ef60212f8e162e94318d6e93b59cbc38e44ab8c
Alessandro Renzi <alessandrorenzi00@gmail.com>
2022-06-21 10:15:20 +0200

    Implementazione dei metodi set e get

--- Attivita/Ricevuta.py
+++ Attivita/Ricevuta.py
@@ -10,4 +10,25 @@ class Ricevuta:
         self.data = datetime.date(1970, 1, 1)
         self.ora = datetime.time(0, 0)
 
-    def
+    def getId(self):
+        return self.id
+
+    def setImporto(self, importo):
+        self.importo = importo
+
+    def getImporto(self):
+        return self.importo
+
+    def setData(self, date):
+        self.data = date;
+
+    def getData(self):
+        return self.date
+
+    def setOra(self, ora):
+        self.ora = ora;
+
+    def getOra(self):
+        return self.ora
+
+
