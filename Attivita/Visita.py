4cce69377aef4f1ba356a42c3cbd2d4fc4f28618
Alessandro Renzi <alessandrorenzi00@gmail.com>
2022-06-21 10:33:47 +0200

    Definizione metodi get e set

---
+++ Attivita/Visita.py
@@ -0,0 +1,19 @@
+import datetime
+import os
+import pickle
+
+class Visita:
+
+    def getId(self):
+        return self.id
+
+    def getNome(self):
+        return self.nome
+
+    def setNota(self, nota):
+        self.nota = nota
+
+    def getNota(self):
+        return self.nota;
+
+
