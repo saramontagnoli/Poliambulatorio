f31a4491615bb5e2ab33a7ac78fa39fd62743e2e
PCristian00 <cristianp9300@gmail.com>
2022-06-22 11:31:46 +0200

    Password in stringa ma invisibile

--- Viste/VistaLogin.py
+++ Viste/VistaLogin.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QSizePolicy)
 
 
 class VistaLogin(QWidget):
@@ -6,17 +6,29 @@ class VistaLogin(QWidget):
         super().__init__()
         self.setWindowTitle('Login')
         self.resize(350, 200)
-        layout = QGridLayout()
+        grid_layout = QGridLayout()
         label1 = QLabel('<font size="8"> UserId </font>')
         self.user_obj = QLineEdit()
-        layout.addWidget(label1, 0, 0)
-        layout.addWidget(self.user_obj, 0, 1)
+        grid_layout.addWidget(label1, 0, 0)
+        grid_layout.addWidget(self.user_obj, 0, 1)
         label2 = QLabel('<font size="8"> Password </font>')
         self.user_pwd = QLineEdit()
         # impostazioni della visibilita password
         self.user_pwd.setEchoMode(QLineEdit.Password)
-        layout.addWidget(label2, 1, 0)
-        layout.addWidget(self.user_pwd, 1, 1)
-        button_login = QPushButton('Login')
-        layout.addWidget(button_login, 2, 0, 2, 2)
-        self.setLayout(layout)
+        grid_layout.addWidget(label2, 1, 0)
+        grid_layout.addWidget(self.user_pwd, 1, 1)
+        # salvataggio in stringa
+        password = self.user_pwd.text()
+        print("Password: "+password)
+        grid_layout.addWidget(self.get_generic_button("Login", self.go_login, password), 2, 0, 2, 2)
+        self.setLayout(grid_layout)
+
+    # Funzionalità del bottone
+    def get_generic_button(self, titolo, on_click, password):
+        button = QPushButton(titolo)
+        button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        button.clicked.connect(lambda: on_click(password))
+        return button
+
+    def go_login(self, password):
+        print(str(password))
