629c78346d134670ce7a3a08b9db7adeda5efcbb
saramontagnoli <s1098041@studenti.univpm.it>
2022-07-16 15:19:29 +0200

    Fix errori id_reparto nelle informazioni dei Medici

--- Viste/VistaInserisciMedici.py
+++ Viste/VistaInserisciMedici.py
@@ -1,9 +1,3 @@
-from datetime import datetime
-
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QCheckBox, QRadioButton
-#from PyQt5.uic.properties import QtWidgets
-
 from Attivita.Medico import Medico
 import os
 import pickle
