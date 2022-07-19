720d12350c11c916c277ac445da75ecdcc6fd876
saramontagnoli <s1098041@studenti.univpm.it>
2022-07-19 15:03:42 +0200

    Visualizzazione informazioni medico

--- Viste/VistaHomeMedico.py
+++ Viste/VistaHomeMedico.py
@@ -14,7 +14,8 @@ class VistaHomeMedico(QWidget):
         grid_layout.addWidget(self.get_generic_button("Vedi Prenotazioni", self.go_prenotazioni), 0, 0)
         grid_layout.addWidget(self.get_generic_button("Vedi Turni", self.go_turni), 0, 1)
         grid_layout.addWidget(self.get_generic_button("Modifica Informazioni", self.go_informazioni), 1, 0)
-        grid_layout.addWidget(self.get_generic_button("Logout", self.go_logout), 1, 1)
+        grid_layout.addWidget(self.get_generic_button("Visualizza Informazioni", self.go_vis_info), 1, 1)
+        grid_layout.addWidget(self.get_generic_button("Logout", self.go_logout), 2, 0, 1, 2)
         self.setLayout(grid_layout)
         self.resize(400, 300)
         self.setWindowTitle(f"Dot. {self.medico.nome} {self.medico.cognome} - {self.medico.id}")
@@ -39,3 +40,12 @@ class VistaHomeMedico(QWidget):
 
     def go_logout(self):
         pass
+
+    def go_vis_info(self):
+        messaggio = QMessageBox()
+        messaggio.setWindowIcon(QIcon('CroceVerde.png'))
+        messaggio.setWindowTitle("Profilo")
+        messaggio.setText(f"Id: {self.medico.id} \nNome: {self.medico.nome}€ \nCognome: {self.medico.cognome} \nCF: {self.medico.CF} \nData nascita: {self.medico.data_nascita} \nGenere: {self.medico.genere} \nIndirizzo: {self.medico.indirizzo}\nMail: {self.medico.mail} \nTelefono: {self.medico.telefono} \nReparto: {self.medico.id_reparto} \nAbilitazione: {self.medico.abilitazione} \nNota: {self.medico.nota}" )
+        messaggio.setText()
+        messaggio.exec_()
+        return
