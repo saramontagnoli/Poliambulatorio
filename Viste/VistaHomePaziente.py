509d35b503e536208a36da713e695750b15f8ca6
PCristian00 <cristianp9300@gmail.com>
2022-07-19 15:23:22 +0200

    Migliorie visualizza info

--- Viste/VistaHomePaziente.py
+++ Viste/VistaHomePaziente.py
@@ -7,14 +7,14 @@ from Viste.VistaModificaPaziente import VistaModificaPaziente
 
 class VistaHomePaziente(QWidget):
 
-    #Vista che rappresenta la home del paziente con i relativi pulsanti
-    def __init__(self, paziente, parent = None):
+    # Vista che rappresenta la home del paziente con i relativi pulsanti
+    def __init__(self, paziente, parent=None):
         super(VistaHomePaziente, self).__init__(parent)
         self.paziente = paziente
         self.setWindowIcon(QIcon('CroceVerde.png'))
         grid_layout = QGridLayout()
         print(self.paziente.CF)
-        grid_layout.addWidget(self.get_generic_button("Vedi Prenotazioni",self.go_prenotazioni), 0, 0)
+        grid_layout.addWidget(self.get_generic_button("Vedi Prenotazioni", self.go_prenotazioni), 0, 0)
         grid_layout.addWidget(self.get_generic_button("Modifica Informazioni", self.go_modifica), 0, 1)
         grid_layout.addWidget(self.get_generic_button("Visualizza Informazioni", self.go_vis_info), 1, 0)
         grid_layout.addWidget(self.get_generic_button("Logout", self.go), 1, 1)
@@ -22,7 +22,7 @@ class VistaHomePaziente(QWidget):
         self.resize(400, 300)
         self.setWindowTitle(f"{self.paziente.nome} {self.paziente.cognome} - {self.paziente.id}")
 
-    #Funzione che crea i ulsanti presenti nella home
+    # Funzione che crea i ulsanti presenti nella home
     def get_generic_button(self, titolo, on_click):
         button = QPushButton(titolo)
         button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -41,13 +41,20 @@ class VistaHomePaziente(QWidget):
         pass
 
     def go_vis_info(self):
+
         messaggio = QMessageBox()
         messaggio.setWindowIcon(QIcon('CroceVerde.png'))
         messaggio.setWindowTitle("Profilo")
-        messaggio.setText(f"Id: {self.paziente.id} \nNome: {self.paziente.nome} \nCognome: {self.paziente.cognome} \nCF: {self.paziente.CF} \nData nascita: {self.paziente.data_nascita.strftime('%Y-%m-%d')} \nGenere: {self.paziente.genere} \nIndirizzo: {self.paziente.indirizzo}\nMail: {self.paziente.mail} \nTelefono: {self.paziente.telefono} \nNota: {self.paziente.nota} \nAllergia: {self.paziente.allergia} \nMalattia pregressa: {self.paziente.malattia_pregressa}" )
+
+        allergia_str = "No"
+        malattia_pregressa_str = "No"
+        if self.paziente.allergia:
+            allergia_str = "Si"
+
+        if self.paziente.malattia_pregressa:
+            malattia_pregressa_str = "Si"
+
+        messaggio.setText(
+            f"Id: {self.paziente.id} \nNome: {self.paziente.nome} \nCognome: {self.paziente.cognome} \nCF: {self.paziente.CF} \nData nascita: {self.paziente.data_nascita.strftime('%Y-%m-%d')} \nGenere: {self.paziente.genere} \nIndirizzo: {self.paziente.indirizzo}\nMail: {self.paziente.mail} \nTelefono: {self.paziente.telefono} \nNota: {self.paziente.nota} \nAllergia: {allergia_str} \nMalattia pregressa: {malattia_pregressa_str}")
         messaggio.exec_()
         return
-
-
-
-
