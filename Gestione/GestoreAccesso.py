c715421fffe1adcd33272f5b54989ba249bb20a4
saramontagnoli <s1098041@studenti.univpm.it>
2022-07-20 23:32:25 +0200

    Sistemazione codice, organizzazione dei commenti e formalizzazione del package Gestione

--- Gestione/GestoreAccesso.py
+++ Gestione/GestoreAccesso.py
@@ -1,3 +1,9 @@
+"""
+    Classe che implementa la gestione degli accessi alla piattaforma
+    Rappresenta la gestione della form di login, smistando i vari livelli di accesso e i diversi utenti (pazienti e medici)
+    e il superutente (l'amministratore)
+"""
+
 from Gestione.GestoreFile import caricaFile
 from Viste.VistaHomeAmm import VistaHomeAmm
 from Viste.VistaHomeMedico import VistaHomeMedico
@@ -5,17 +11,34 @@ from Viste.VistaHomePaziente import VistaHomePaziente
 
 
 class GestoreAccesso:
+
+    """
+        Costruttore della classe
+        Definisco la vista home a None
+    """
     def __init__(self):
         self.vista_home = None
 
-    # Gestione dei livelli di accesso degli utenti della piattaforma
+
+    """
+        Metodo che permette la gestione del login alla piattaforma del poliambulatorio.
+        Controllo:
+            - se sta accedendo l'amministratore (admin)
+            - se sta accedendo un paziente (CF del paziente)
+            - se sta accedendo un medico (CF del medico)
+        In base all'utente che accede alla piattaforma (se i dati sono corretti) si apre la relativa vista dell'area privata 
+        con tutte le funzionalità da poter svolgere
+    """
     def login(self, username, password):
-        # Controllo se l'utente che vuole accedere è l'admin (superutente)
+
+        # controllo se è l'admin a voler accedere (superutente)
         if username == "admin" and password == "admin":
+            # apro la vista dell'admin
             self.vista_home = VistaHomeAmm()
             self.vista_home.show()
             return True
 
+        # controllo se è un medico a voler accedere
         medici = caricaFile("Medici")
         for medico in medici:
             if medico.CF == username and medico.password == password:
@@ -24,6 +47,7 @@ class GestoreAccesso:
                 self.vista_home.show()
                 return True
 
+        # controllo se è un paziente a voler accedere
         pazienti = caricaFile("Pazienti")
         for paziente in pazienti:
             if paziente.CF == username and paziente.password == password:
--- Gestione/GestoreFile.py
+++ Gestione/GestoreFile.py
@@ -1,20 +1,37 @@
+"""
+    Classe che implementa la gestione dei caricamenti e scritture su File
+    Si occupa anche delle ricerche effettuate sui File
+    Per rendere più generale il gestore si passa come parametro il nome del file, creando il path
+"""
+
 import os
 import pickle
 import shutil
 
 
+"""
+    La cartella Appoggio contiene tutti i file vuoti e i file di Reparti e Visite precompilati
+    Nel momento in cui dovessero essere cancellati i file dal package File si provvede al caricamento dei file "base"
+    direttamente copiandoli da Appoggio a File
+    Metodo, dunque, che permette il caricamento dei file di default nel momento in cui non dovessero essere presenti, dovessero
+    essere stati cancellati o corrotti e quindi avessero bisogno di un ripristino.
+"""
 def creazioneFile(filename):
     empty_list = []
     path1 = f"Appoggio/{filename}.pickle"
     path2 = f"File/{filename}.pickle"
     print(path1)
+    # se il file passato come parametro non esiste lo carica da Appoggio, se esiste lo mantiene
     if not os.path.exists(path2):
         shutil.copy(path1, 'File/')
 
 
-
+"""
+    Metodo che permette la ricerca di un determinato elemento a partire dall'id, in un file passato come parametro
+"""
 def ricercaElemFile(filename, id):
     path = f"File/{filename}.pickle"
+    # apertura del file e caricamento dei dati nel dizionario
     if os.path.isfile(path):
         with open(path, 'rb') as f:
             dizionario = dict(pickle.load(f))
@@ -22,37 +39,49 @@ def ricercaElemFile(filename, id):
     else:
         return None
 
-
+"""
+    Metodo che permette di eseguire il caricamento di un file passato come parametro in un dizionario
+    Return di tutto il dizionario riempito
+"""
 def caricaFile(filename):
     dizionario = []
     path = f"File/{filename}.pickle"
-    # Apertura e scrittura su file delle visite
+    # apertura del file e caricamento dei dati nel dizionario
     if os.path.isfile(path):
         with open(path, 'rb') as f:
             current = dict(pickle.load(f))
             dizionario.extend(current.values())
         return dizionario
 
-
+"""
+    Metodo che permette di eseguire la scrittura di un elemento nel file passato come parametro
+    Return True se ho portato a termine l'inserimento in modo corretto 
+"""
 def scriviFile(filename, elemento):
     dizionario = {}
     path = f"File/{filename}.pickle"
 
+    # apertura file per il caricamento delle informazioni
     if os.path.isfile(path):
         with open(path, 'rb') as f:
             dizionario = pickle.load(f)
     dizionario[elemento.id] = elemento
+    # apertura del file in scrittura e aggiunta delle nuove informazioni nel file
     with open(path, 'wb') as f:
         pickle.dump(dizionario, f, pickle.HIGHEST_PROTOCOL)
         return True
 
-
+"""
+    Metodo che permette di eseguire la rimozione di un elemento da un file passato come parametro
+"""
 def rimuoviElemFile(filename, elemento):
     path = f"File/{filename}.pickle"
+    # apertura del file per il caricamento dei dati e rimozione dell'elemento
     if os.path.isfile(path):
         with open(path, 'rb') as f:
             dizionario = dict(pickle.load(f))
             del dizionario[elemento.id]
+        # apertura in scrittura del file e caricamento del dizionario modificato nel file
         with open(path, 'wb') as f:
             pickle.dump(dizionario, f, pickle.HIGHEST_PROTOCOL)
 
