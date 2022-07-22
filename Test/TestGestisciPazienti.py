e3f5b9bbfb0508f6691abc20effa6ed11444d59c
PCristian00 <cristianp9300@gmail.com>
2022-07-22 11:55:07 +0200

    TestGestisciPazienti funzionante

--- Test/TestGestisciPazienti.py
+++ Test/TestGestisciPazienti.py
@@ -6,67 +6,36 @@ from unittest import TestCase
 
 from Attivita.Paziente import Paziente
 
-from Gestione.GestoreFile import caricaFile, creazioneFile
-
 
 class TestGestisciPazienti(TestCase):
 
     def test_add_paziente(self):
-        creazioneFile("Pazienti")
 
-        """"
-        os.makedirs("File/")
-        empty_list = []
-        # Open the pickle file in 'wb' so that you can write and dump the empty variable
-        openfile = open('File/Pazienti.pickle', 'wb')
-        pickle.dump(empty_list, openfile)
-        openfile.close()
-        """
         self.paziente = Paziente()
         self.paziente.setInfoPaziente(1, "Nome", "Cognome", "Password", datetime.datetime(2001, 1, 1), "CF",
-                                      "Telefono","M", "mail", "Indirizzo", "Nota", True, True)
+                                      "Telefono", "M", "mail", "Indirizzo", "Nota", True, True)
 
-        pazienti = caricaFile("Pazienti")
-
-        """"
         pazienti = None
         if os.path.isfile('File/Pazienti.pickle'):
             with open('File/Pazienti.pickle', 'rb') as f:
                 pazienti = pickle.load(f)
-                
-        print(pazienti[0])
-        print(pazienti[0].nome)
-        """
 
         self.assertIsNotNone(pazienti)
-
-        for paziente in pazienti:
-            print("\n")
-            print(paziente.id)
-            print(paziente.nome)
-            print(paziente.cognome)
-            print(paziente.password)
-            print(paziente.data_nascita)
-            print("\n")
-            # self.assertIn(paziente.id, pazienti)
 
         self.assertIn(1, pazienti)
 
     def test_rimuovi_paziente(self):
-        pazienti = caricaFile("Pazienti")
-        """"
         pazienti = None
         if os.path.isfile('File/Pazienti.pickle'):
             with open('File/Pazienti.pickle', 'rb') as f:
                 pazienti = pickle.load(f)
-"""
         self.assertIsNotNone(pazienti)
         self.assertIn(1, pazienti)
         self.paziente = Paziente().ricercaUtilizzatoreId(1)
         self.paziente.rimuoviPaziente()
-
-        pazienti = caricaFile("Pazienti")
-
+        if os.path.isfile('File/Pazienti.pickle'):
+            with open('File/Pazienti.pickle', 'rb') as f:
+                pazienti = pickle.load(f)
         self.assertIsNotNone(pazienti)
         self.assertNotIn(1, pazienti)
 
